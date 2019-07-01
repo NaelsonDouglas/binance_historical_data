13e5a6a7de57a4623ce523ad47d1b8b501d01cde
Naelson Douglas C. Oliveira <naelson17@gmail.com>
2019-07-01 04:20:24 -0300

    Update main.py
    Push forçado porque o github deu o cão

--- src/main.py
+++ src/main.py
@@ -8,43 +8,44 @@ import time
 import logging
 
 
-
 logging.basicConfig(filename='log.log',
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-tick_time = 60*5 #seconds
-timestamp = datetime.datetime.now().timestamp()
-timestamp = datetime.datetime.fromtimestamp(timestamp).isoformat()
+tick_time = 5 #seconds
 
 
-volume = requests.get('https://www.binance.com/exchange/public/product')
-price = requests.get('https://www.binance.com/api/v1/ticker/allBookTickers')
-while volume.status_code  != 200 or price.status_code != 200:
-    if volume.status_code  != 200:
-        logging.debug('The request for volume returned '+str(volume.status_code))
-    if price.status_code  != 200:
-        logging.debug('The request for price returned '+str(price.status_code))
+while True:
 
     timestamp = datetime.datetime.now().timestamp()
     timestamp = datetime.datetime.fromtimestamp(timestamp).isoformat()
+
     volume = requests.get('https://www.binance.com/exchange/public/product')
     price = requests.get('https://www.binance.com/api/v1/ticker/allBookTickers')
-    time.sleep(10)
+    while volume.status_code  != 200 or price.status_code != 200:
+        if volume.status_code  != 200:
+            logging.debug('The request for volume returned '+str(volume.status_code))
+        if price.status_code  != 200:
+            logging.debug('The request for price returned '+str(price.status_code))
+        timestamp = datetime.datetime.now().timestamp()
+        timestamp = datetime.datetime.fromtimestamp(timestamp).isoformat()
+        volume = requests.get('https://www.binance.com/exchange/public/product')
+        price = requests.get('https://www.binance.com/api/v1/ticker/allBookTickers')
+        time.sleep(10)
 
-#The volumeuest returns a json with a single line 'data' which is a dictionary
-#Then I convert this dictionary to a dataframe
-volume = volume.json()['data']
-volume = pd.DataFrame(volume)
-volume['bidPrice']=-1
-volume['bidQty']=-1
-volume['askPrice']=-1
-volume['askQty']=-1
+    #The volume request returns a json with a single line 'data' which is a dictionary
+    #Then I convert this dictionary to a dataframe
+    volume = volume.json()['data']
+    volume = pd.DataFrame(volume)
+    volume['bidPrice']=-1
+    volume['bidQty']=-1
+    volume['askPrice']=-1
+    volume['askQty']=-1
 
-price = price.json()
-price = pd.DataFrame(price)
-while True:
+    price = price.json()
+    price = pd.DataFrame(price)
+
     for pair in volume['symbol']:
             try: #There are some pairs thar aren't being shown on  https://www.binance.com/api/v1/ticker/allBookTickers. GTOPAX is one of them at this time
                 line = volume[volume.symbol==pair]
@@ -67,10 +68,10 @@ while True:
             f.close()
 
     logging.info('Syncing on Git')
-    subprocess.run("git add ../dumps/*",shell=True)
-    subprocess.run("git pull",shell=True) #This pull will be useful when we have more than one server runing
-    subprocess.run("git commit -m "+'"'+timestamp+'"',shell=True)
-    subprocess.run("git push",shell=True)
+    #subprocess.run("git add ../dumps/*",shell=True)
+    #subprocess.run("git pull",shell=True) #This pull will be useful when we have more than one server runing
+    #subprocess.run("git commit -m "+'"'+timestamp+'"',shell=True)
+    #subprocess.run("git push",shell=True)
     logging.info('Commit '+timestamp+' pushed to git')
   
     logging.info('Sleeping for '+str(tick_time/60)+' minute(s)')
